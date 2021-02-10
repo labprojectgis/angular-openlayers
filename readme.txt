fb0e9ecd074d761f59380796400d03dcc6f4a922
labprojectgis <lab.project.gis@gmail.com>
2021-02-10 15:51:21 +0000

    basic map

--- readme.txt
+++ readme.txt
@@ -1,3 +1,8 @@
 npm install -g @angular/cli
 npm install --global npm@6
 ng new angular-openlayers --directory ./
+
+npm install ol --save
+npm install @types/ol  --save
+npm install proj4 --save
+npm install @types/proj4 --save
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OlMapComponent } from './ol-map/ol-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OlMapComponent
   ],
   imports: [
     BrowserModule
